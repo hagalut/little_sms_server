a335cc03e79be4517c041a2edc4b29b2f3b46502
izbrannick <izbrannick@gmail.com>
2015-04-03 11:05:51 +0200

    Major changes - transfering from INBOX handling to OnReceive handling - no tested for mass smses yet
    pending work - testing sms with huge number of receivers and setting async background on sms sending

--- app/src/main/java/dk/glutter/izbrannick/nativesmsforwarder/Sms.java
+++ app/src/main/java/dk/glutter/izbrannick/nativesmsforwarder/Sms.java
@@ -2,17 +2,27 @@ package dk.glutter.izbrannick.nativesmsforwarder;
 
 public class Sms {
 
-	private String _id;
+    private String _id;
+    private String _thread_id;
 	private String _address;
 	private String _msg;
-	private String _readState; // "0" for have not read sms and "1" for have
-								// read sms
+    // READ STATE - "0" for have not read sms and "1" for have read sms
+	private String _readState;
 	private String _time;
 	private String _folderName;
+    private String _messageType;
+    public static final int MESSAGE_TYPE_SMS = 1;
+    public static final int MESSAGE_TYPE_MMS = 2;
 
-	public String getId() {
-		return _id;
-	}
+    public String getId() {
+        return _id;
+    }
+    public String getThreadId() {
+        return _thread_id;
+    }
+    public void setThreadId(String thread_id) {
+        _thread_id = thread_id;
+    }
 
 	public String getAddress() {
 		return _address;
@@ -58,4 +68,11 @@ public class Sms {
 		_folderName = folderName;
 	}
 
+    public String get_messageType() {
+        return _messageType;
+    }
+
+    public void set_messageType(String _messageType) {
+        this._messageType = _messageType;
+    }
 }
